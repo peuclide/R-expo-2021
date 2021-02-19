fc2aabf6e641a9e7e934ddab3590e334911afd1f
Peter Euclide <peuclide@users.noreply.github.com>
2021-02-19 12:34:48 -0600

    test new page

--- build_site.R
+++ build_site.R
@@ -3,5 +3,5 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-source('~/OneDrive - UWSP/UWSP_Larson_2018/projects/R_workshop/Rexpo_2021/rexpo_tutorials/build_site.R')#render your sweet site. 
+source('~/OneDrive - UWSP/UWSP_Larson_2018/projects/R_workshop/Rexpo_2021/rexpo_tutorials/R-expo-2021/build_site.R')#render your sweet site. 
 rmarkdown::render_site()
